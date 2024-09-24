d1798c5039e80483363bee9972b33ab9a719e1b7
BlessingOkora <moraablessing082@gmail.com>
2024-09-24 10:07:32 +0300

    Modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+const mondayWork = function(toDo = "go to the office") {
+    return `This Monday, I will ${toDo}.`;
+};
+function wrapAdjective(cool = "*") {
+    return function(adjective = "special")
+    {
+        return `You are ${cool}${adjective}${cool}!`;
+    };
+}
